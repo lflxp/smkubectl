2c2ba7ffbd89211af3f1494ad0e8118bd87ddb8a
lflxp <382023823@qq.com>
2024-01-30 19:22:30 +0800

    zsh completion bugfix

--- cmd/zsh.go
+++ cmd/zsh.go
@@ -11,7 +11,7 @@ import (
 
 const zshCompletion = `compdef _k k                                                                                                                                                                                                                                                                    # 添加 _gmt 函数
 
-__cobra-cli_debug()
+__smkubectl-cli_debug()
 {
     local file="$BASH_COMP_DEBUG_FILE"
     if [[ -n ${file} ]]; then
@@ -25,14 +25,14 @@ _k() {
   # 获取一次性结果
   result=$(command smkubectl ${LBUFFER})
 
-  __cobra-cli_debug "namespace ${result}"
+  __smkubectl-cli_debug "namespace ${result}"
   namespace=$(command echo ${result}|head -1|awk '/^NAMESPACE/ {print "yes"}')
-  __cobra-cli_debug "namespace ${namespace}"
+  __smkubectl-cli_debug "namespace ${namespace}"
   # 根据标题是否含有NAMESPACE动态切换显示结果
   if [[ -n "$namespace" ]]; then
-    matches=$(command echo ${result} | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" fzf -m|awk '{print "-n "$1" "$2}'|tr '\n' ' ')
+    matches=$(command echo ${result} | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --header-lines=1 --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" fzf -m|awk '{print "-n "$1" "$2}'|tr '\n' ' ')
   else
-    matches=$(command echo ${result} | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" fzf -m|awk '{print $1}'|tr '\n' ' ')
+    matches=$(command echo ${result} | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --header-lines=1 --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" fzf -m|awk '{print $1}'|tr '\n' ' ')
   fi
   #_describe 'command' ns
   if [ -n "$matches" ]; then
